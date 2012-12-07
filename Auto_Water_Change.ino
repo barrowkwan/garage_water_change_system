0b59e0fccd0b6b48e64473fa6e063bc1cc66eaff
Barrow Kwan <barrowkwan@gmail.com>
2012-12-07 13:19:27 -0800

    Stable version One - without LCD and DHT

--- Auto_Water_Change.ino
+++ Auto_Water_Change.ino
@@ -24,9 +24,22 @@
 #include <Time.h>
 #include <TimeAlarms.h>
 
-DHT dht(2, 22);
-LiquidCrystal_I2C lcd(0x27,16,2);
+
+#define DS1307_I2C_ADDRESS 0x68  // This is the I2C address
+// Arduino version compatibility Pre-Compiler Directives
+#if defined(ARDUINO) && ARDUINO >= 100   // Arduino v1.0 and newer
+  #define I2C_WRITE Wire.write 
+  #define I2C_READ Wire.read
+#else                                   // Arduino Prior to v1.0 
+  #define I2C_WRITE Wire.send 
+  #define I2C_READ Wire.receive
+#endif
+
+
+//DHT dht(2, 22);
+//LiquidCrystal_I2C lcd(0x27,16,2);
 int needFilterWaterMonitor = 1;
+
 
 const int relayPin1 = 4;
 const int relayPin2 = 5;
@@ -35,14 +48,56 @@ const int relayPin4 = 7;
 const int relayPin5 = 8;
 const int floatValveHighPin = 3;
 const int floatValveLowPin = 9;
+const char  *Mon[] = {"","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+
+
+String getDateTime(int withDate=1){
+  /*
+  char  *Mon[] = {"","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+  char timestring[22];
+  snprintf(timestring,sizeof(timestring),"%02d:%02d:%02d %02d %s %4d",hour(),minute(),second(),day(),Mon[month()],year());
+  return String(timestring);
+  */
+  String timestring = "";
+  int sec, min, hr, dayOfMonth, mon, yr;
+
+  sec     = second();
+  min     = minute();
+  hr       = hour();  // Need to change this if 12 hour am/pm
+  dayOfMonth = day();
+  mon      = month();
+  yr       = year();
+  
+  
+    if (hr < 10)
+    timestring += "0";
+  timestring += hr;
+  timestring += ":";
+  if (min < 10)
+    timestring += "0";
+  timestring += min;
+  timestring += ":";
+  if (sec < 10)
+    timestring += "0";
+  timestring += sec;
+  if (withDate == 1){
+    timestring += " ";
+    timestring += dayOfMonth;
+    timestring += " ";
+    timestring += Mon[mon];
+    timestring += " ";
+    timestring += yr;
+  }
+  return timestring;
+}
 
 void setup()
 {
   byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x45, 0x1D };
   byte ip[] = { 192, 168, 210, 52 };
-  byte twitter_server[] = { 128, 121, 146, 100 };
+  //byte twitter_server[] = { 128, 121, 146, 100 };
 
-  Serial.begin(9600);
+  Serial.begin(57600);
   Wire.begin();
   setSyncProvider(RTC.get);
   if(timeStatus()!= timeSet) 
@@ -65,22 +120,28 @@ void setup()
   digitalWrite(relayPin4, 0);
   digitalWrite(relayPin5, 1);
 
+
+/*
   lcd.init();
   lcd.backlight();
   lcd.clear();
   lcd.print("Start...");
-
+*/
+  
   //Serial.println(String(RTC.get));
   Alarm.timerRepeat(600,PostTwitterStatus);
 
   // create the alarms 
-  Alarm.alarmRepeat(9,30,0, waterChange);  
+  Alarm.alarmRepeat(12,3,0, waterChange);
+  Alarm.alarmRepeat(20,3,0, waterChange);
+  Alarm.alarmRepeat(4,3,0, waterChange);
 //  Alarm.alarmRepeat(dowSaturday,8,30,30,WeeklyAlarm);  // 8:30:30 every Saturday 
 //  Alarm.timerRepeat(15, Repeats);            // timer for every 15 seconds    
 //  Alarm.timerOnce(10, OnceOnly);             // called once after 10 seconds 
 
 
-  postTwitter("Garage water change system started : " + getTime());
+  postTwitter("Garage water change system started : " + getDateTime(1));
+
 
 }
 
@@ -102,67 +163,61 @@ void waterChange(){
     6. full?? turn clean water off
     7. turn clean water on after xxxxx second and enable fillwater ( alarm.timerOnce(.... ); )
   */
-  int jobInt = 0;
-  lcd.setCursor(0,1);
-  lcd.print("WC@ " + getTimeOnly());
-  postTwitter("Start water change : " + getTime());
+
+
+/*  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("WC@ " + getDateTime(0)); */
+  postTwitter("Start water change : " + getDateTime());
+
+
   needFilterWaterMonitor = 0;
-  jobInt = jobInt + 10;
-  Alarm.timerOnce(jobInt,turnFilteredWaterOff);
-  jobInt = jobInt + 2;
-  Alarm.timerOnce(jobInt,turnFilteredWaterOff);
-  jobInt = jobInt + 5;
-  Alarm.timerOnce(jobInt,turnWaterPumpOn);
-  jobInt = jobInt + 2;
-  Alarm.timerOnce(jobInt,turnWaterPumpOn);
-  jobInt = jobInt + 180;
-  Alarm.timerOnce(jobInt,turnWaterPumpOff);
-  jobInt = jobInt + 2;
-  Alarm.timerOnce(jobInt,turnWaterPumpOff);
-  jobInt = jobInt + 5;
-  Alarm.timerOnce(jobInt,turnCleanWaterOn);
-  jobInt = jobInt + 2;
-  Alarm.timerOnce(jobInt,turnCleanWaterOn);
-  jobInt = jobInt + 180;
-  Alarm.timerOnce(jobInt,turnCleanWaterOff);
-  jobInt = jobInt + 2;
-  Alarm.timerOnce(jobInt,turnCleanWaterOff);
-  jobInt = jobInt + 3600;
-  Alarm.timerOnce(jobInt, turnCleanWaterOnAndStartMonitor);
-  jobInt = jobInt + 2;
-  Alarm.timerOnce(jobInt, turnCleanWaterOnAndStartMonitor);
+  Alarm.delay(10000);
+  turnFilteredWaterOff();
+
+
+  Alarm.delay(5000);
+  turnWaterPumpOn();
+  Alarm.delay(180000);
+  turnWaterPumpOff();
+  Alarm.delay(5000);
+  turnCleanWaterOn();
+  Alarm.delay(180000);
+  turnCleanWaterOff();
+  Alarm.delay(3600000);
+  turnCleanWaterOnAndStartMonitor();
 }
 
 void turnFilteredWaterOff(){
-  postTwitter("Turn filtered water off : " + getTime());
+  postTwitter("Turn filtered water off : " + getDateTime());
   digitalWrite(relayPin1, 1);
   digitalWrite(relayPin2, 0);
 }
 
 void turnWaterPumpOn(){
-  postTwitter("Turn water pump on : " + getTime());
+  postTwitter("Turn water pump on : " + getDateTime());
   digitalWrite(relayPin5, 0);
 }
 
 void turnWaterPumpOff(){
-  postTwitter("Turn water pump off : " + getTime());
+  postTwitter("Turn water pump off : " + getDateTime());
   digitalWrite(relayPin5, 1);
 }
 
 void turnCleanWaterOn(){
-  postTwitter("Turn clean water on : " + getTime());
+  postTwitter("Turn clean water on : " + getDateTime());
   digitalWrite(relayPin3, 0);
   digitalWrite(relayPin4, 1);
 }
 
 void turnCleanWaterOff(){
-  postTwitter("Turn clean water off : " + getTime());
+  postTwitter("Turn clean water off : " + getDateTime());
   digitalWrite(relayPin3, 1);
   digitalWrite(relayPin4, 0);
 }
 
 void turnCleanWaterOnAndStartMonitor(){
-  postTwitter("Turn clean water on after water is warm : " + getTime());
+  postTwitter("Turn clean water on after water is warm : " + getDateTime());
   needFilterWaterMonitor = 1;
   digitalWrite(relayPin3, 0);
   digitalWrite(relayPin4, 1);
@@ -176,13 +231,13 @@ void waterMonitor(){
    */
   if (needFilterWaterMonitor == 1){
     if ((digitalRead(floatValveHighPin) == 0) && (digitalRead(floatValveLowPin) == 0)){
-      Serial.println("Water monitor turn clean water on : " + getTime());
+      Serial.println("Water monitor turn clean water on : " + getDateTime());
       digitalWrite(relayPin3, 0);
       digitalWrite(relayPin4, 1);
       
     }
     if (digitalRead(floatValveHighPin) && digitalRead(floatValveLowPin)){
-      Serial.println("Water monitor turn clean water off : " + getTime());
+      Serial.println("Water monitor turn clean water off : " + getDateTime());
       digitalWrite(relayPin3, 1);
       digitalWrite(relayPin4, 0);
     }
@@ -194,12 +249,13 @@ void digitalClockDisplay()
 {
   // digital clock display of the time
   
-  /*Serial.print(hour());
+  Serial.print(hour());
   printDigits(minute());
   printDigits(second());
-  Serial.println();*/
-  lcd.setCursor(0,0);
-  lcd.print(getTimeOnly());
+  Serial.println();
+
+/*  lcd.setCursor(0,0);
+  lcd.print(getDateTime(0)); */
 }
 
 void printDigits(int digits)
@@ -229,7 +285,7 @@ void PostTwitterStatus(){
   //Serial.println("Status : " + getTime() + " | " + String(temp) + " | " + String(humidstr));
   postTwitter("Status : " + getTime() + " | " + String(temp) + " | " + String(humidstr));
   */
-  postTwitter("Status : " + getTime());
+  postTwitter("Status : " + getDateTime());
 }
 
 void postTwitter(String msg){
@@ -242,7 +298,7 @@ void postTwitter(String msg){
     int status = twitter.wait(&Serial);
     
     if (status == 200) {
-      Serial.println("Tweets : " + getTime());
+      Serial.println("Tweets : " + getDateTime());
     } else {
       Serial.println("Twitter failed :" + status);
     }
@@ -250,20 +306,14 @@ void postTwitter(String msg){
     Serial.println("Twitter connection failed.");
   }
   Alarm.delay(1000);
-}
-
-String getTime(){
-  char  *Mon[] = {"","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-  char timestring[22];
-  snprintf(timestring,sizeof(timestring),"%02d:%02d:%02d %02d %s %4d",hour(),minute(),second(),day(),Mon[month()],year());
-  return String(timestring);
+  Serial.println(msg);
 }
 
 
-String getTimeOnly(){
-  char timestring[9];
-  snprintf(timestring,sizeof(timestring),"%02d:%02d:%02d",hour(),minute(),second());
-  return String(timestring);
-}
 
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return ( (val/16*10) + (val%16) );
+}
 
